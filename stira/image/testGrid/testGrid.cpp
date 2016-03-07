29f8ded668833bc4f66402df1125352238d26c54
frooms <filip.rooms@gmail.com>
2016-03-07 12:46:39 +0100

    Added PCA visualization experiment

--- stira/image/testGrid/testGrid.cpp
+++ stira/image/testGrid/testGrid.cpp
@@ -17,6 +17,7 @@
 #include <time.h>
 #include <opencv2/opencv.hpp>
 #include "../../common/common/Point.h"
+#include "../../common/common/DrawFigures.h"
 #include "../tools/GridExtender.h"
 #include "../tools/GridGenerator.h"
 #include "../tools/DrawImageTools.h"
@@ -334,17 +335,52 @@ bool LocalStatisticsTest()
    return true;
 }
 
-double TestOrientationPCA()
+void drawAxis(cv::Mat& img, cv::Point p, cv::Point q, cv::Scalar colour, const float scale = 0.2)
 {
-    /*vector<Point> pts;
-    cv::Mat img;
+    double angle;
+    double hypotenuse;
+    angle = atan2( (double) p.y - q.y, (double) p.x - q.x ); // angle in radians
+    hypotenuse = sqrt( (double) (p.y - q.y) * (p.y - q.y) + (p.x - q.x) * (p.x - q.x));
+
+    // Here we lengthen the arrow by a factor of scale
+    q.x = (int) (p.x - scale * hypotenuse * cos(angle));
+    q.y = (int) (p.y - scale * hypotenuse * sin(angle));
+    line(img, p, q, colour, 1, CV_AA);
+    // create the arrow hooks
+    p.x = (int) (q.x + 9 * cos(angle + CV_PI / 4));
+    p.y = (int) (q.y + 9 * sin(angle + CV_PI / 4));
+    line(img, p, q, colour, 1, CV_AA);
+    p.x = (int) (q.x + 9 * cos(angle - CV_PI / 4));
+    p.y = (int) (q.y + 9 * sin(angle - CV_PI / 4));
+    line(img, p, q, colour, 1, CV_AA);
+}
+
+bool TestOrientationPCA()
+{
+    vector< stira::common::Point<double> > pts;
+    /* initialize random seed: */
+    srand (time(NULL));
+
+    for (int i = 0; i < 500; i++)
+    {
+          double x = 512 * (double)(rand()) / ((double)RAND_MAX);
+          double y = 512 * (double)(rand()) / ((double)RAND_MAX);
+
+          if ( DrawFigures::IsInsideEllipse(x, y, 256, 256, 200, 50, M_PI / 4) )
+          {   stira::common::Point<double> pt(x, y);
+              pts.push_back( pt );
+          }
+    }
+
+    cv::Mat img(512, 512, CV_8UC3);
     //Construct a buffer used by the pca analysis
     int sz = static_cast<int>(pts.size());
     cv::Mat data_pts = cv::Mat(sz, 2, CV_64FC1);
     for (int i = 0; i < data_pts.rows; ++i)
     {
-        data_pts.at<double>(i, 0) = pts[i].x;
-        data_pts.at<double>(i, 1) = pts[i].y;
+        data_pts.at<double>(i, 0) = pts[i].GetX();
+        data_pts.at<double>(i, 1) = pts[i].GetY();
+        cv::circle(img, cv::Point( pts[i].GetX(), pts[i].GetY()), 3, cv::Scalar(255, 255, 255), 1);
     }
     //Perform PCA analysis
     cv::PCA pca_analysis(data_pts, cv::Mat(), CV_PCA_DATA_AS_ROW);
@@ -359,18 +395,23 @@ double TestOrientationPCA()
         eigen_vecs[i] = cv::Point2d(pca_analysis.eigenvectors.at<double>(i, 0),
                                     pca_analysis.eigenvectors.at<double>(i, 1));
         eigen_val[i] = pca_analysis.eigenvalues.at<double>(0, i);
-    }*/
-    // Draw the principal components
-    //circle(img, cntr, 3, Scalar(255, 0, 255), 2);
-    //Point p1 = cntr + 0.02 * Point(static_cast<int>(eigen_vecs[0].x * eigen_val[0]), static_cast<int>(eigen_vecs[0].y * eigen_val[0]));
-    //Point p2 = cntr - 0.02 * Point(static_cast<int>(eigen_vecs[1].x * eigen_val[1]), static_cast<int>(eigen_vecs[1].y * eigen_val[1]));
-    //drawAxis(img, cntr, p1, Scalar(0, 255, 0), 1);
-    //drawAxis(img, cntr, p2, Scalar(255, 255, 0), 5);
-    //double angle = atan2(eigen_vecs[0].y, eigen_vecs[0].x); // orientation in radians
-    //return angle;
-    return 1.0;
-}
+    }
 
+    cout << "PCA center = ( " << cntr.x << ", " << cntr.y << endl;
+    cout << "       eigen vector 1 = (" << eigen_vecs[0].x  << ", " << eigen_vecs[0].y << ")  value = " << eigen_val[0] << endl;
+    cout << "       eigen vector 2 = (" << eigen_vecs[1].x  << ", " << eigen_vecs[1].y << ")  value = " << eigen_val[1] << endl;
+
+    // Draw the principal components
+    cv::circle(img, cntr, 3, cv::Scalar(255, 0, 255), 2);
+    cv::Point p1 = cntr + 0.02 * cv::Point(static_cast<int>(eigen_vecs[0].x * eigen_val[0]), static_cast<int>(eigen_vecs[0].y * eigen_val[0]));
+    cv::Point p2 = cntr - 0.02 * cv::Point(static_cast<int>(eigen_vecs[1].x * eigen_val[1]), static_cast<int>(eigen_vecs[1].y * eigen_val[1]));
+    drawAxis(img, cntr, p1, cv::Scalar(0, 255, 0), 1);
+    drawAxis(img, cntr, p2, cv::Scalar(255, 255, 0), 5);
+    double angle = atan2(eigen_vecs[0].y, eigen_vecs[0].x); // orientation in radians
+    cout << "       angle = " << angle << endl;
+    cv::imwrite("PcaOut.png", img);
+    return true;
+}
 
 //--------------------------------------------------------------------
 
@@ -456,6 +497,11 @@ int main( )
    if( GridAverageTest() != true )
    {
       cerr << "TestAverageGrid failed" << endl << flush;
+   }
+
+   if (TestOrientationPCA() != true )
+   {
+      cerr << "TestOrientationPCA failed" << endl << flush;
    }
    return 0;
 }
