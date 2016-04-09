0698cd7f2d3215bd307116122b79b4570fbd8696
frooms <filip.rooms@gmail.com>
2016-04-09 14:50:49 +0200

    initial empty implementation

--- stira/contrastenhance/contrastenhance/AdaptiveHistogramEqualizer.cpp
+++ stira/contrastenhance/contrastenhance/AdaptiveHistogramEqualizer.cpp
@@ -1,7 +1,59 @@
 #include "AdaptiveHistogramEqualizer.h"
 
+#include "HistogramTools.h"
+#include "../../common/common/MathUtils.h"
+#include "../../filter/filter/GaussConvolve.h"
+#include "../../histogram/histogram/IntHistogram.h"
+#include "../../histogram/histogram/FloatHistogram.h"
+#include "../../image/tools/NumberGridTools.h"
+
+namespace stira {
+namespace contrastenhance {
+
 AdaptiveHistogramEqualizer::AdaptiveHistogramEqualizer()
 {
-
 }
 
+/*
+void AdaptiveHistogramEqualizer::HistogramEqualizeSingleBlock( image::ArrayGrid<double>* pInGrid, int xi, int yi  )
+{
+   int desiredMax, desiredMin;
+   double dataMin, dataMax;
+   common::RectangularROI<int> rroi();
+
+   NumberGridTools<double>::GetMinMax( pInGrid, dataMin, dataMax );
+   bool useDataMinMax = false;
+   IntHistogram* pStdHistogram       = new IntHistogram( pInGrid, useDataMinMax, rroi );
+   IntHistogram* pCumulHistogram     = new IntHistogram( pInGrid, useDataMinMax, rroi );
+   FloatHistogram *pNormCumulHistogram = new FloatHistogram( pStdHistogram->GetNrOfBins() );
+
+   pCumulHistogram->ConvertInCumulativeHistogram();
+
+   pNormCumulHistogram->InitializeWithNormalizedCumulativeHistogram( pStdHistogram, pCumulHistogram);
+   delete pStdHistogram;
+   delete pCumulHistogram;
+
+   desiredMax = (int)(dataMax);
+   desiredMin = 0;
+
+   for (int y = 0; y < pInGrid->GetHeight(); y++)
+   {
+      for (int x = 0; x < pInGrid->GetWidth(); x++)
+      {
+         int binNr = pInGrid->GetValue(x, y) - dataMin;
+
+         double myEqualizedValue = desiredMax * ( pNormCumulHistogram->GetBinValue( 0, binNr ) / ((double)(pNormCumulHistogram->GetNrOfBins())));
+
+         if (myEqualizedValue > static_cast<double>(desiredMax)) {myEqualizedValue = static_cast<double>(desiredMax);}
+         if (myEqualizedValue < static_cast<double>(desiredMin)) {myEqualizedValue = static_cast<double>(desiredMin);}
+
+         pInGrid->SetValue(x, y, myEqualizedValue);
+      }
+   }
+
+   delete pNormCumulHistogram;
+}
+   */
+
+}
+}
--- stira/contrastenhance/contrastenhance/AdaptiveHistogramEqualizer.h
+++ stira/contrastenhance/contrastenhance/AdaptiveHistogramEqualizer.h
@@ -1,15 +1,53 @@
-#ifndef ADAPTIVEHISTOGRAMEQUALIZER_H
-#define ADAPTIVEHISTOGRAMEQUALIZER_H
 
+/***************************************************************************************
+ *   Copyright (C) 2016 by Filip Rooms                                                 *
+ *                                                                                     *
+ *  Terms and conditions for using this software in any form are provided in the       *
+ *  file COPYING, which can be found in the root directory of this project.            *
+ *                                                                                     *
+ *   Contact data: filip.rooms@gmail.com                                               *
+ *                 http://www.filiprooms.be/                                           *
+ *                                                                                     *
+ ***************************************************************************************/
+
+#ifndef STIRA_CONTRASTENHANCE_ADAPTIVEHISTOGRAMEQUALIZER_H
+#define STIRA_CONTRASTENHANCE_ADAPTIVEHISTOGRAMEQUALIZER_H
+
+#include "../../common/common/RectangularROI.h"
+#include "../../image/datastructures/Image.h"
+#include "../../histogram/histogram/FloatHistogram.h"
+
+namespace stira {
+namespace contrastenhance {
+
+/** \brief class with tools for image intensity remapping using histograms, like histogram matching and histogram equalization
+  */
 
 class AdaptiveHistogramEqualizer
 {
 public:
     AdaptiveHistogramEqualizer();
 
-signals:
+    void HistogramEqualizeSingleBlock( image::ArrayGrid<double>* pInGrid, int xi, int yi );
 
-public slots:
+
+    /** \brief overloaded Run method per image
+      * \param pSourceImage the input image
+      * \return a newly created image as result of contrast enhancement, becomes responsability of the caller*/
+    image::Image* Run( image::Image* pSourceImage );
+
+
+    /** \brief sets the size of the local window around a central pixel
+      * \param size size of the local window around a central pixel
+      * usually between 5 and 11 */
+    void SetBlockSize( int size );
+
+private:
+    image::ArrayGrid<histogram::FloatHistogram*>* pHistogramPerBlock;
+
 };
+
+}
+}
 
 #endif // ADAPTIVEHISTOGRAMEQUALIZER_H
