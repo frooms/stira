4206edf052e5e0ace2eed703290d6f0da440202a
frooms <filip.rooms@gmail.com>
2016-04-11 19:11:03 +0200

    New class to implement Burt and Adelson Pyramid

---
+++ stira/steerable/pyramid/PyramidBurtAdelson.cpp
@@ -0,0 +1,7 @@
+#include "PyramidBurtAdelson.h"
+
+PyramidBurtAdelson::PyramidBurtAdelson()
+{
+
+}
+
---
+++ stira/steerable/pyramid/PyramidBurtAdelson.h
@@ -0,0 +1,15 @@
+#ifndef PYRAMIDBURTADELSON_H
+#define PYRAMIDBURTADELSON_H
+
+
+class PyramidBurtAdelson
+{
+public:
+    PyramidBurtAdelson();
+
+signals:
+
+public slots:
+};
+
+#endif // PYRAMIDBURTADELSON_H
