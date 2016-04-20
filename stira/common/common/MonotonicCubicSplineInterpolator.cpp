6759a541581a2de1365e9b935716dc47b366cdf9
frooms <filip.rooms@gmail.com>
2016-04-20 23:28:01 +0200

    Subclassed QLabel to intercept click events on a QLabel to be able to interact with images inside

---
+++ stira/common/common/MonotonicCubicSplineInterpolator.cpp
@@ -0,0 +1,7 @@
+#include "MonotonicCubicSplineInterpolator.h"
+
+MonotonicCubicSplineInterpolator::MonotonicCubicSplineInterpolator()
+{
+
+}
+
---
+++ stira/common/common/MonotonicCubicSplineInterpolator.h
@@ -0,0 +1,15 @@
+#ifndef MONOTONICCUBICSPLINEINTERPOLATOR_H
+#define MONOTONICCUBICSPLINEINTERPOLATOR_H
+
+
+class MonotonicCubicSplineInterpolator
+{
+public:
+    MonotonicCubicSplineInterpolator();
+
+signals:
+
+public slots:
+};
+
+#endif // MONOTONICCUBICSPLINEINTERPOLATOR_H
