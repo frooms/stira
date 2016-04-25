a53f7a0273846dfc1f8bb2967499231c36361b59
frooms <filip.rooms@gmail.com>
2016-04-25 18:09:56 +0200

    Allowing user interaction with window by emitting signals from derived class ClickableLabel from QLabel; and adding it to the project file

--- gui/src/InteractiveImageWindow.cpp
+++ gui/src/InteractiveImageWindow.cpp
@@ -1,7 +1,296 @@
+
+/****************************************************************************
+ **
+ ** Copyright (C) 2005-2007 Trolltech ASA. All rights reserved.
+ **
+ ** This file is part of the example classes of the Qt Toolkit.
+ **
+ ** This file may be used under the terms of the GNU General Public
+ ** License version 2.0 as published by the Free Software Foundation
+ ** and appearing in the file LICENSE.GPL included in the packaging of
+ ** this file.  Please review the following information to ensure GNU
+ ** General Public Licensing requirements will be met:
+ ** http://www.trolltech.com/products/qt/opensource.html
+ **
+ ** If you are unsure which license is appropriate for your use, please
+ ** review the following information:
+ ** http://www.trolltech.com/products/qt/licensing.html or contact the
+ ** sales department at sales@trolltech.com.
+ **
+ ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ **
+ ****************************************************************************/
+
+/***********************************************************************************
+ *   Copyright (C) 2008 by Filip Rooms (modifications)                             *
+ *                                                                                 *
+ *  Terms and conditions for using this software in any form are provided in the   *
+ *  file COPYING, which can be found in the root directory of this project.        *
+ *                                                                                 *
+ *   Contact data: filip.rooms@gmail.com                                           *
+ *                 http://www.filiprooms.be/                                       *
+ *                                                                                 *
+ ***********************************************************************************/
+
+#include <QtGui>
+#include <string>
+#include <QMainWindow>
+#include <QSlider>
+#include <iostream>
+
+#include "ProcessManager.h"
 #include "InteractiveImageWindow.h"
+#include "MainWindow.h"
+#include "ImageDataList.h"
+#include "../../stira/common/common/MathUtils.h"
+#include "../../stira/common/common/StringUtils.h"
+#include "../../stira/image/tools/ImageTools.h"
 
-InteractiveImageWindow::InteractiveImageWindow()
+using namespace std;
+using namespace stira::image;
+using namespace stira::common;
+
+//------------------------------------------------------------------
+
+InteractiveImageWindow::InteractiveImageWindow (  double centerX, double centerY, double width )
 {
+   InitializePointers( );
+   mpFractalGenerator = new GenerateFractal();
+   mpImage = mpFractalGenerator->CreateMandelbrot( centerX, centerY, width );
+   if ( mpImage != 0)
+   {
+      SetupWindow();
+      mpQImage = ImageDataList::ImageToQImage ( mpImage );
+      SetupImage( );
+      this->show();
+   }
+}
 
+//------------------------------------------------------------------
+
+InteractiveImageWindow::~InteractiveImageWindow( )
+{
+    SlotCleanData();
+}
+
+//------------------------------------------------------------------
+
+void InteractiveImageWindow::InitializePointers( )
+{
+   mpImage = 0;
+   mpQImage = 0;
+   mpParentWindow = 0;
+   mIsFirstRendering = true;
+}
+
+//------------------------------------------------------------------
+
+void InteractiveImageWindow::SetupImage( )
+{
+   if (mpQImage->format() != QImage::Format_RGB32)
+   {
+      QImage convertedImg = mpQImage->convertToFormat (QImage::Format_RGB32);
+      delete mpQImage;
+      mpQImage = new QImage( convertedImg );
+   }
+   RenderImage ( mpQImage );
+}
+
+//------------------------------------------------------------------
+
+void InteractiveImageWindow::CreateZoomFractal( double factor )
+{
+    Point<double> mathCenter = mpFractalGenerator->GetMathCenterPoint();
+    int resolutionX = mpFractalGenerator->GetResolutionX();
+    int resolutionY = mpFractalGenerator->GetResolutionY();
+    cout << "resolutionX = " << resolutionX << endl;
+
+    Point<int> clickPoint = mpImageLabel->GetPointClicked();
+
+    double mathClickX = (double)(clickPoint.x - mpFractalGenerator->GetPixelWidth()  / 2.0 ) / (double)(resolutionX) + mathCenter.x;
+    double mathClickY = (double)(clickPoint.y - mpFractalGenerator->GetPixelHeight() / 2.0 ) / (double)(resolutionY) + mathCenter.y;
+
+    cout << "Previous mathCenter = (" << mathCenter.x << ", " << mathCenter.y << ")" << endl;
+
+    cout << "clickPoint = (" << clickPoint.x << ", " << clickPoint.y << ")" << endl;
+    cout << "mathClick = (" << mathClickX << ", " << mathClickY << ")" << endl;
+
+    // full size
+    double width = mpFractalGenerator->GetMathWidth() / factor;
+
+    Image* pFractal = mpFractalGenerator->CreateMandelbrot( mathClickX, mathClickY, width );
+    this->ReplaceImage(  pFractal );
+}
+
+//------------------------------------------------------------------
+
+void InteractiveImageWindow::ReplaceImage( Image* pNewImage )
+{
+   if (mpImage != 0) {delete mpImage; }
+   mpImage = pNewImage;
+
+   QImage* pQImageOld =  mpQImage;
+   mpQImage = ImageDataList::ImageToQImage ( mpImage );
+   delete pQImageOld;
+   if (mpQImage->format() != QImage::Format_RGB32)
+   {
+      QImage convertedImg = mpQImage->convertToFormat (QImage::Format_RGB32);
+      delete mpQImage;
+      mpQImage = new QImage( convertedImg );
+   }
+   RenderImage ( mpQImage );
+}
+
+//------------------------------------------------------------------
+
+QMdiSubWindow* InteractiveImageWindow::GetParent()
+{
+   return mpParentWindow;
+}
+
+//------------------------------------------------------------------
+
+void InteractiveImageWindow::SetParent( QMdiSubWindow* pParentWindow )
+{
+   mpParentWindow = pParentWindow;
+}
+
+//------------------------------------------------------------------
+
+Image* InteractiveImageWindow::GetImage( )
+{
+   return mpImage;
+}
+
+//------------------------------------------------------------------
+
+void InteractiveImageWindow::SetupWindow()
+{
+   mpImageLabel = new ClickableLabel(this);//QLabel;
+   mpImageLabel->setBackgroundRole ( QPalette::Base );
+   mpImageLabel->setSizePolicy ( QSizePolicy::Ignored, QSizePolicy::Ignored );
+   mpImageLabel->setScaledContents ( true );
+
+   mpScrollArea = new QScrollArea;
+   mpScrollArea->setBackgroundRole ( QPalette::Dark );
+   mpScrollArea->setWidget ( mpImageLabel );
+
+   setWindowTitle ( tr ( "Image Viewer" ) );
+
+   connect( mpImageLabel, SIGNAL( wheelUpEvent() ), this, SLOT( SlotZoomIn() ));
+   connect( mpImageLabel, SIGNAL( wheelDownEvent() ), this, SLOT( SlotZoomOut() ));
+
+   mpSlider = new QSlider( Qt::Horizontal, this );
+   mpSlider->hide();
+
+   mpWidgetsLayout = new QVBoxLayout;
+   mpWidgetsLayout->addWidget( mpScrollArea );
+   mpWidgetsLayout->addWidget( mpSlider );
+   mpWindowLayout = new QGridLayout( this );
+   mpWindowLayout->addLayout( mpWidgetsLayout, 0, 0  );
+}
+
+//------------------------------------------------------------------
+
+void InteractiveImageWindow::SlotCleanData()
+{
+    if (mpQImage != 0)
+    {
+       delete mpQImage;
+    }
+
+    if (mpImage != 0)
+    {
+       delete mpImage;
+    }
+
+    delete mpFractalGenerator;
+}
+
+//------------------------------------------------------------------
+
+void InteractiveImageWindow::SlotSetSizeCondition()
+{
+   if ( mpImage->GetWidth() < StiraDefinitions::maxWidth )
+   {
+      setMinimumWidth ( mpImage->GetWidth() + 13 );
+   }
+   else
+   {
+      setMaximumWidth ( StiraDefinitions::maxWidth );
+      setMinimumWidth ( StiraDefinitions::maxWidth );
+   }
+
+   if ( mpImage->GetHeight() < StiraDefinitions::maxHeight )
+   {
+      setMinimumHeight ( mpImage->GetHeight() + 38 );
+   }
+   else
+   {
+      setMaximumHeight ( StiraDefinitions::maxHeight );
+      setMinimumHeight ( StiraDefinitions::maxHeight );
+   }
+   SlotNormalSize();
+}
+
+//------------------------------------------------------------------
+
+void InteractiveImageWindow::SlotNormalSize()
+{
+   mpScrollArea->setWidgetResizable ( false );
+   mpImageLabel->adjustSize();
+   mScaleFactor = 1.0;
+   this->update();
+}
+
+//------------------------------------------------------------------
+
+void InteractiveImageWindow::SlotZoomIn()
+{
+    CreateZoomFractal( 2.0 );
+}
+
+//------------------------------------------------------------------
+
+void InteractiveImageWindow::SlotZoomOut()
+{
+    CreateZoomFractal( 0.5 );
+}
+
+//------------------------------------------------------------------
+
+void InteractiveImageWindow::RenderImage ( QImage* pQImage )
+{
+   if ( pQImage->isNull() )
+   {
+      QMessageBox::information ( this, tr ( "Image Viewer" ), tr ( "Cannot load." ) );
+      return;
+   }
+
+   if (mIsFirstRendering == true)
+   {
+      SlotSetSizeCondition();
+      mIsFirstRendering = false;
+   }
+
+   mpImageLabel->setPixmap ( QPixmap::fromImage ( *pQImage ) );
+
+   ScaleImage ( );
+}
+
+//------------------------------------------------------------------
+
+void InteractiveImageWindow::ScaleImage ( )
+{
+   mpImageLabel->resize ( mScaleFactor * mpImageLabel->pixmap()->size() );
+}
+
+//------------------------------------------------------------------
+
+void InteractiveImageWindow::AdjustScrollBar ( QScrollBar *scrollBar, double factor )
+{
+   scrollBar->setValue ( int ( factor * scrollBar->value()
+                              + ( ( factor - 1 ) * scrollBar->pageStep() /2 ) ) );
 }
 
--- gui/src/InteractiveImageWindow.h
+++ gui/src/InteractiveImageWindow.h
@@ -1,15 +1,113 @@
-#ifndef INTERACTIVEIMAGEWINDOW_H
-#define INTERACTIVEIMAGEWINDOW_H
+
+/****************************************************************************
+ **
+ ** Copyright (C) 2005-2007 Trolltech ASA. All rights reserved.
+ **
+ ** This file is part of the example classes of the Qt Toolkit.
+ **
+ ** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ ** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ **
+ ****************************************************************************/
+
+/***********************************************************************************
+ *   Copyright (C) 2008 by Filip Rooms        (modifications)                      *
+ *                                                                                 *
+ *  Terms and conditions for using this software in any form are provided in the   *
+ *  file COPYING, which can be found in the root directory of this project.        *
+ *                                                                                 *
+ *   Contact data: filip.rooms@gmail.com                                           *
+ *                 http://www.filiprooms.be/                                       *
+ *                                                                                 *
+ ***********************************************************************************/
+#ifndef STIRAGUI_MAIN_INTERACTIVEIMAGEWINDOW_H
+#define STIRAGUI_MAIN_INTERACTIVEIMAGEWINDOW_H
+
+#include <QMainWindow>
+#include <QDialog>
+#include <QPrinter>
+#include <QMenuBar>
+#include <QMdiSubWindow>
+#include <QVBoxLayout>
+#include <QContextMenuEvent>
+#include <QFrame>
+
+#include "ClickableLabel.h"
+#include "../../stira/image/datastructures/Image.h"
+#include "../../stira/image/tools/GenerateFractal.h"
+
+class ProcessManager;
+
+class QAction;
+class QLabel;
+class QMenu;
+class QScrollArea;
+class QScrollBar;
+class QSlider;
+
+class MainWindow;
 
 
-class InteractiveImageWindow
+/** \brief a widget to interactively view a fractal image in the GUI
+  * Is embedded in an MDI subwindow */
+class InteractiveImageWindow : public QWidget
 {
-public:
-    InteractiveImageWindow();
+   Q_OBJECT
 
-signals:
+public:
+
+   /** \brief constructor
+     * Creates an image viewer with an externally created fractal image */
+   InteractiveImageWindow( double centerX, double centerY, double width );
+
+   /** \brief destructor */
+   ~InteractiveImageWindow( );
+
+   stira::image::Image* GetImage( );
+
+   QMdiSubWindow* GetParent();
+   void SetParent( QMdiSubWindow* pParentWindow );
 
 public slots:
+   void SlotZoomIn();
+   void SlotZoomOut();
+   void SlotSetSizeCondition();
+   void SlotNormalSize();
+   void SlotCleanData();
+private:
+
+   /** \brief sets all member pointers initially to "0" */
+   void InitializePointers( );
+   void SetupWindow();
+   void SetupImage( );
+   void RenderImage ( QImage* pQImage );
+   void ReplaceImage( stira::image::Image* pNewImage );
+
+   void CreateZoomFractal(double factor );
+
+   bool mIsFirstRender;
+
+  // WAS ALREADY IN ORIGINAL IMAGE VIEWER
+  ///////////////////////////////////////
+
+   void ScaleImage( );
+   void AdjustScrollBar(QScrollBar *scrollBar, double factor);
+
+   ClickableLabel* mpImageLabel;
+   QImage* mpQImage;
+   QMdiSubWindow* mpParentWindow;
+   QScrollArea* mpScrollArea;
+   double mScaleFactor;
+
+   QVBoxLayout* mpWidgetsLayout;
+   QGridLayout* mpWindowLayout;
+   QSlider* mpSlider;  ///< slider to control checker size or transparancy amaount
+
+   bool mIsFirstRendering;  ///< keeps track if this is the first time that the image is rendered in the window
+
+   stira::image::Image* mpImage;  ///< the underlying image data of image window
+   stira::image::GenerateFractal* mpFractalGenerator;  ///< the underlying image data of image window
 };
+
 
 #endif // INTERACTIVEIMAGEWINDOW_H
