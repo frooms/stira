25657ac4fb27e216b16bb59b840c74e4116f7cf9
frooms <filip.rooms@gmail.com>
2016-05-19 00:52:21 +0200

    Added references

--- stira/imageanalysis/imageanalysis/StereoMatchDepth.h
+++ stira/imageanalysis/imageanalysis/StereoMatchDepth.h
@@ -21,24 +21,26 @@
 namespace stira {
 namespace imageanalysis {
 
+// http://www.cse.psu.edu/~rtc12/CSE486/lecture09_6pp.pdf
+// http://campar.in.tum.de/twiki/pub/Chair/TeachingWs09Cv2/3D_CV2_WS_2009_Stereo.pdf
 class StereoMatchDepth
 {
 public:
     StereoMatchDepth();
 
-    static image::ArrayGrid<double>* MatchStereo(image::ArrayGrid<double>* pGrid1, image::ArrayGrid<double>* pGrid2,
-                                          int windowHalfWidth, common::MatchCriterion myMatchCriterion, int maxDisparity );
+    static image::ArrayGrid<double>* MatchStereo( image::ArrayGrid<double>* pGrid1, image::ArrayGrid<double>* pGrid2,
+                                                  int windowHalfWidth, common::MatchCriterion myMatchCriterion, int maxDisparity );
 
 private:
 
-    static  image::ArrayGrid<double>* MatchStereoSSD( image::ArrayGrid<double>* pGrid1, image::ArrayGrid<double>* pGrid2, int windowHalfWidth,
-                                                      int maxDisparity );
+    static image::ArrayGrid<double>* MatchStereoSSD( image::ArrayGrid<double>* pGrid1, image::ArrayGrid<double>* pGrid2,
+                                                     int windowHalfWidth, int maxDisparity );
 
-     static image::ArrayGrid<double>* MatchStereoNCC( image::ArrayGrid<double>* pGrid1, image::ArrayGrid<double>* pGrid2, int windowHalfWidth,
-                                                      int maxDisparity );
+    static image::ArrayGrid<double>* MatchStereoNCC( image::ArrayGrid<double>* pGrid1, image::ArrayGrid<double>* pGrid2,
+                                                     int windowHalfWidth, int maxDisparity );
 
-     static image::ArrayGrid<double>* MatchStereoMI( image::ArrayGrid<double>* pGrid1, image::ArrayGrid<double>* pGrid2, int windowHalfWidth,
-                                                     int maxDisparity );
+    static image::ArrayGrid<double>* MatchStereoMI( image::ArrayGrid<double>* pGrid1, image::ArrayGrid<double>* pGrid2,
+                                                    int windowHalfWidth, int maxDisparity );
 };
 
 }
