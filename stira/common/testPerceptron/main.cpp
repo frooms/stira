a280fd3d91d4e7d0b848e3611aac0633a80fc9dc
Filip Rooms <filip.rooms@gmail.com>
2017-01-12 17:20:57 +0100

    added test executable

--- stira/common/testPerceptron/main.cpp
+++ stira/common/testPerceptron/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include "../common/Perceptron.h"
+#include "../common/TextFileUtils.h"
 using namespace std;
+using namespace stira::common;
 
 int main()
 {
+    std::vector<ClassSample> samples = TextFileUtils::ReadDataSamplesFromTextFile( std::string("../../testdata/IrisDataset.txt"), 0, 1 );
+
     cout << "Hello World!" << endl;
     return 0;
 }
